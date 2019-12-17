ba9217ec4dfc37f5af28ad4d497eb2315f5bec58
Shang <shang1219178163@gmail.com>
2019-12-17 14:26:38 +0800

    update

--- HFNavigationController.podspec
+++ HFNavigationController.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'HFNavigationController'
-  s.version          = '1.0.1'
+  s.version          = '1.0.2'
   s.summary          = "like Apple' signIn UX"
 
 # This description is used to generate tags and improve search results.
--- UserDefaults+Ex.swift
+++ UserDefaults+Ex.swift
@@ -1,5 +1,5 @@
 //
-//  NSUserDefaults+Ex.swift
+//  UserDefaults+Ex.swift
 //  HFNavigationController
 //
 //  Created by Bin Shang on 2019/12/17.
@@ -7,15 +7,15 @@
 
 import UIKit
 
-@objc extension UserDefaults{
+@objc public extension UserDefaults{
     ///UserDefaults 存档对象(别忘了 synchronize)
-    public static func setArcObject(_ value: Any?, forkey defaultName: String) {
+    static func setArcObject(_ value: Any?, forkey defaultName: String) {
         guard let value = value else { return }
         let data = NSKeyedArchiver.archivedData(withRootObject: value)
         standard.setValue(data, forKey: defaultName)
     }
     ///UserDefaults 解档对象
-    public static func unarcObject(forkey defaultName: String) -> Any? {
+    static func unarcObject(forkey defaultName: String) -> Any? {
         guard let value = standard.object(forKey: defaultName) as? Data else { return nil}
         return NSKeyedUnarchiver.unarchiveObject(with: value);
     }
