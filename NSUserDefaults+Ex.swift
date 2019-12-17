0233e02f36e04ac9c3530e29c6958512f4ce1437
Shang <shang1219178163@gmail.com>
2019-12-17 14:20:39 +0800

    update

--- NSUserDefaults+Ex.swift
+++ NSUserDefaults+Ex.swift
@@ -9,13 +9,13 @@ import UIKit
 
 @objc extension UserDefaults{
     ///UserDefaults 存档对象(别忘了 synchronize)
-    static func setArcObject(_ value: Any?, forkey defaultName: String) {
+    public static func setArcObject(_ value: Any?, forkey defaultName: String) {
         guard let value = value else { return }
         let data = NSKeyedArchiver.archivedData(withRootObject: value)
         standard.setValue(data, forKey: defaultName)
     }
     ///UserDefaults 解档对象
-    static func unarcObject(forkey defaultName: String) -> Any? {
+    public static func unarcObject(forkey defaultName: String) -> Any? {
         guard let value = standard.object(forKey: defaultName) as? Data else { return nil}
         return NSKeyedUnarchiver.unarchiveObject(with: value);
     }
