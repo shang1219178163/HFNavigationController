03ec97093f94b33e3026f22e6d5db9f9cfe5f524
Shang <shang1219178163@gmail.com>
2021-06-30 17:25:44 +0800

    添加 popover 呈现

--- Example/HFNavigationController/PopoverDemoController.swift
+++ Example/HFNavigationController/PopoverDemoController.swift
@@ -12,19 +12,53 @@ class PopoverDemoController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        edgesForExtendedLayout = []
+        view.backgroundColor = UIColor.systemGreen
+        
+        title = "popover 呈现"
+        
+        navigationItem.rightBarButtonItems = ["done"].map({
+            let sender = UIButton(type: .custom)
+            sender.setTitle($0, for: .normal)
+            sender.setTitleColor(.black, for: .normal)
+            sender.addTarget(self, action: #selector(handlecAction(_:)), for: .touchUpInside)
+            return UIBarButtonItem(customView: sender)
+        })
+    }
 
-        // Do any additional setup after loading the view.
+    @objc func handlecAction(_ sender: UIButton) {
+        let vc = UIViewController()
+        vc.view.backgroundColor = UIColor.blue
+        
+        vc.modalPresentationStyle = .popover
+        vc.preferredContentSize = CGSize(width: 300, height: 600)
+
+        let ppc = vc.popoverPresentationController
+//        ppc?.permittedArrowDirections = .any
+        ppc?.permittedArrowDirections = UIPopoverArrowDirection.init(rawValue: 0)
+
+        ppc?.sourceView = sender
+        ppc?.sourceRect = sender.frame
+
+        ppc?.delegate = self
+        present(vc, animated: true, completion: nil)
+    }
+
+
+}
+
+
+extension PopoverDemoController: UIPopoverPresentationControllerDelegate{
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .none
+    }
+
+    func adaptivePresentationStyle(for controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
+        return .none
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func popoverPresentationControllerShouldDismissPopover(_ popoverPresentationController: UIPopoverPresentationController) -> Bool {
+        return true
     }
-    */
-
 }
