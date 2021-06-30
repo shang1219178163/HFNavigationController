6df1d6c9ecc38cb89d2aba5b095ffa487a9815ae
Shang <shang1219178163@gmail.com>
2021-06-30 17:25:26 +0800

    新增默认 popover 呈现

---
+++ Example/HFNavigationController/PopoverDemoController.swift
@@ -0,0 +1,30 @@
+//
+//  PopoverDemoController.swift
+//  HFNavigationController_Example
+//
+//  Created by Bin Shang on 2021/6/30.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class PopoverDemoController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
